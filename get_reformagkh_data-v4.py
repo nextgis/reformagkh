5b8de3fb4fa2e7f675361768670c0057772e38ca
Gleb Suvorov <suvorov.gleb@gmail.com>
2017-03-22 18:31:49 +0300

    ssssssss

--- get_reformagkh_data-v4.py
+++ get_reformagkh_data-v4.py
@@ -174,8 +174,7 @@ def get_data_links(id):
 
 def check_captcha(soup):
     captcha = soup.find('form', { 'name' : 'request_limiter_captcha'})
-    address_p = soup.find('span', { 'class' : 'float-left loc_name_ohl width650 word-wrap-break-word' })
-    if captcha != None or u'Каптча' in soup.text or address_p is None or'captcha' in str(soup): 
+    if captcha != None or u'Каптча' in soup.text or 'captcha' in str(soup) or u'Каптча' in str(soup): 
         return True
     else:
         return False
