b590a530f45764cafc63efbe5e28f9205098f9ad
Dmitry Kolesov <kolesov.dm@google.com>
2018-07-05 17:14:50 +0300

    Mock interface for osm data format

--- address_parser/house_parser.py
+++ address_parser/house_parser.py
@@ -18,7 +18,8 @@ from address import (
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Parse reformagkh.ru house data")
-    parser.add_argument('filename', metavar='f', help='Name for data file')
+    parser.add_argument('filename', help='Name for data file')
+    parser.add_argument('format', choices=['reforma', 'osm'], help='Format of data file')
 
     args = parser.parse_args()
     
@@ -35,7 +36,7 @@ def read_csv(filename):
     return data
 
 
-def add_row_to_tree(tree, row):
+def add_reforma_row_to_tree(tree, row):
         region = Region(id=row['region_id'], formalname=row['formalname_region'], shortname=row['shortname_region'])
         area = Area(id=row['area_id'], formalname=row['formalname_area'], shortname=row['shortname_area'])
         city = City(id=row['city_id'], formalname=row['formalname_city'], shortname=row['shortname_city'])
@@ -56,6 +57,14 @@ def main():
     args = parse_args()
 
     filename = args.filename
+    fmt = args.format
+    if fmt == 'reforma':
+        add_row_to_tree = add_reforma_row_to_tree
+    elif fmt == 'osm':
+        raise NotImplementedError()
+    else:
+        raise ValueError('Unknown data file format: %s', (format, ))
+
     data = read_csv(filename)
 
     tree = AddressTree()
